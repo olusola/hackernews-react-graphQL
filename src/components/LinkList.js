5adc29e98a7d492af0cfc1d1d4ba781d263de6d8
olusola <olusola2509@gmail.com>
2017-09-26 14:48:33 +0100

    Hacker news clone using react-graphQL-apollo
    v 0.0.1

---
+++ src/components/LinkList.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react'
+import Link from './Link'
+import { graphql, gql } from 'react-apollo'
+
+class LinkList extends Component {
+
+
+
+
+    _updateCacheAfterVote = (store, createVote, linkId) => {
+  // 1
+  const data = store.readQuery({ query: ALL_LINKS_QUERY })
+
+  // 2
+  const votedLink = data.allLinks.find(link => link.id === linkId)
+  votedLink.votes = createVote.link.votes
+
+  // 3
+  store.writeQuery({ query: ALL_LINKS_QUERY, data })
+}
+
+  render() {
+
+    // 1
+    if (this.props.allLinksQuery && this.props.allLinksQuery.loading) {
+      return <div>Loading</div>
+    }
+
+    // 2
+    if (this.props.allLinksQuery && this.props.allLinksQuery.error) {
+      return <div>Error</div>
+    }
+
+    // 3
+    const linksToRender = this.props.allLinksQuery.allLinks
+
+    return (
+      <div>
+          {linksToRender.map((link, index) => (
+            <Link key={link.id} updateStoreAfterVote={this._updateCacheAfterVote}  index={index} link={link}/>
+          ))}
+      </div>
+    )
+  }
+
+}
+
+export const ALL_LINKS_QUERY = gql`
+  query AllLinksQuery {
+    allLinks {
+      id
+      createdAt
+      url
+      description
+      postedBy {
+        id
+        name
+      }
+      votes {
+        id
+        user {
+          id
+        }
+      }
+    }
+  }
+`
+
+export default graphql(ALL_LINKS_QUERY, { name: 'allLinksQuery' }) (LinkList)
---
+++ src/index.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './components/App'
+import registerServiceWorker from './registerServiceWorker'
+import { BrowserRouter } from 'react-router-dom'
+import { GC_AUTH_TOKEN } from './constants'
+import './styles/index.css'
+// 1
+import { ApolloProvider, createNetworkInterface, ApolloClient } from 'react-apollo'
+
+// 2
+const networkInterface = createNetworkInterface({
+  uri: 'https://api.graph.cool/simple/v1/cj80isig60ohi0106luvybd7e'
+})
+
+networkInterface.use([{
+  applyMiddleware(req, next) {
+    if (!req.options.headers) {
+      req.options.headers = {}
+    }
+    const token = localStorage.getItem(GC_AUTH_TOKEN)
+    req.options.headers.authorization = token ? `Bearer ${token}` : null
+    next()
+  }
+}])
+
+// 3
+const client = new ApolloClient({
+  networkInterface
+})
+
+// 4
+ReactDOM.render(
+  <BrowserRouter>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </BrowserRouter>
+  , document.getElementById('root')
+)
+registerServiceWorker()
